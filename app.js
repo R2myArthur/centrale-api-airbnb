59bcde0680ac996e8e332345437ad65303d44671
Remy Arthur <r.arthur@outlook.fr>
2023-10-07 18:33:08 +0200

    correction de l'application

--- app.js
+++ app.js
@@ -8,30 +8,28 @@ const db = new sqlite3.Database('accommodations.db');
 app.use(cors());
 
 app.get("/api/accomodations", (req, res) => {
-//     res.json("coucou Serveur airBNB!");
-// });
-    db.all(
-        "SELECT * FROM accommodations",
-        (err, rows) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).json({ error: "Internal server error" });
-        } else {
-            const accommodations = rows.map((row) => ({
-            id: row.id,
-            image: row.image,
-            city: {
-                zipCode: row.city_zipCode,
-                name: row.city_name,
-            },
-            price: row.price,
-            rating: row.rating,
-            favourite: !!row.favourite,
-            }));
-            res.json(accommodations);
-        }
-        }
-    );
+  db.all(
+    "SELECT id, image, city_zipCode, city_name, price, rating, favourite FROM accommodations",
+    (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        const accommodations = rows.map((row) => ({
+          id: row.id,
+          image: row.image,
+          city: {
+            zipCode: row.city_zipCode,
+            name: row.city_name,
+          },
+          price: row.price,
+          rating: row.rating,
+          favourite: !!row.favourite,
+        }));
+        res.json(accommodations);
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
