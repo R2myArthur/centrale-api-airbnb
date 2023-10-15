8abc1959cee6ab62c67f0866c841b8e09e3b64d8
Remy Arthur <r.arthur@outlook.fr>
2023-10-15 10:42:51 +0200

    Ajout de la requete get 'detail/id'

--- app.js
+++ app.js
@@ -32,6 +32,33 @@ app.get("/api/accomodations", (req, res) => {
   );
 });
 
+// Récupérer le détail d'un seul logement avec id en paramètre
+app.get("/detail/:id", (req, res) => {
+  db.all(
+    //Rechercher dans la base de donnée tous ,es éléments et filtrer avec le paramètre ID et renvoyer au Front
+    "SELECT id, image, city_zipCode, city_name, price, rating, favourite FROM accommodations WHERE id=" + req.params.id,
+    (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        const accommodations = rows.map((row) => ({
+          id: row.id,
+          image: row.image,
+          city: {
+            zipCode: row.city_zipCode,
+            name: row.city_name,
+          },
+          price: row.price,
+          rating: row.rating,
+          favourite: !!row.favourite,
+        }));
+        res.json(accommodations);
+      }
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Serveur écoutant sur le port ${port}`);
 });
